de0401ddf7706eb648225d4848339d39de06099b
aldomaez <alfredojmartinez@hotmail.com>
2021-07-05 03:12:16 -0400

    Add files via upload
    u2

---
+++ dealebayfinder/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import Post, Profile,ListaBusquedas2,Search2,Seller2,ViableItems10Off
+
+admin.site.register(Post)
+admin.site.register(Profile)
+admin.site.register(ListaBusquedas2)
+admin.site.register(Search2)
+admin.site.register(Seller2)
+admin.site.register(ViableItems10Off)
---
+++ dealebayfinder/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class DealebayfinderConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'dealebayfinder'
